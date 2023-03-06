52e4f273ab3db0aad34406fb47c8c468399e68b1
black_dog <basilkooryshev@gmail.com>
2023-03-06 14:44:14 +0300

    beta.v3

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -168,3 +168,4 @@ class UserProfile(viewsets.ModelViewSet):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
